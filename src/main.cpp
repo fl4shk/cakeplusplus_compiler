c6e99e10a8b21add4e402f13110d4eccefb84aa8
fl4shk <fl4shk@users.noreply.github.com>
2017-12-01 16:42:14 -0500

    stuffs

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,21 @@ extern "C"
 {
 u64 ident_count, num_count;
 
-int real_main(int argc, char** argv)
+void inc_ident_count()
+{
+	//printout("Nice\n");
+	++ident_count;
+}
+
+void inc_num_count()
+{
+	++num_count;
+}
+
+}
+
+
+int main(int argc, char** argv)
 {
 	yylex();
 	//for (s64 i=0; i<3; ++i)
@@ -17,17 +31,4 @@ int real_main(int argc, char** argv)
 	printout("Here is my funky amount of numbers:  ", num_count, 
 		"\n");
 	return 0;
-}
-
-void inc_ident_count()
-{
-	//printout("Nice\n");
-	++ident_count;
-}
-
-void inc_num_count()
-{
-	++num_count;
-}
-
 }
