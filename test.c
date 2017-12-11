944f487e42686f10a7fe4227740da77cf1281011
fl4shk <fl4shk@users.noreply.github.com>
2017-12-11 10:07:17 -0500

    aaaa

--- test.c
+++ test.c
@@ -1,12 +1,18 @@
 {
-	int a[7];
+	int aaaaa[7];
 	int i;
 	i = 0;
 
 	while (i < 7)
 	{
-		a[i] = 6;
-		i = i + 1;
+		int j;
+		j = 0;
+		while (j < 9)
+		{
+			aaaaa[i] = 6;
+			i = i + 1;
+			j = j + 1;
+		}
 	}
 
 }
