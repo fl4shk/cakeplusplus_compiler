c219a8a74943ce167af9e96712146497929aaa37
fl4shk <fl4shk@users.noreply.github.com>
2017-12-17 14:38:41 -0600

    stuffs

--- cake++_vm_assembler/tests/test_1.asm
+++ cake++_vm_assembler/tests/test_1.asm
@@ -1,8 +1,16 @@
 main:
+	; Allocate space for return value
+	const(0)
+
+	; Arguments
 	const(8)
 	const(9)
+
+	; Function label
 	const(add)
 	call
+
+	disp_num
 
 
 	const(0)
@@ -10,18 +18,22 @@ main:
 
 
 add:
-	const_s8(-16)
-	argx
-
-	arg
-	ld_basic
-
 	const_s8(-8)
 	argx
 	ld_basic
 
+	arg
+	ld_basic
+
 	add
 
+	; return value
+	const_s8(-16)
+	argx
 	st_basic
+
+
+	const_s8(-16)
+	add_to_sp
 
 	ret
