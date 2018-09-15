f48f3b1f6cfc7466b9b2abeaaa3392da4e16f39d
fl4shk <fl4shk@users.noreply.github.com>
2018-09-15 13:40:50 -0400

    Memory access helpers

--- cake++_vm/src/vm_class.cpp
+++ cake++_vm/src/vm_class.cpp
@@ -4,6 +4,52 @@
 namespace cakeplusplus
 {
 
+size_t Vm::InstrHeader::num_arg_bytes() const
+{
+	switch (group())
+	{
+		case 0:
+			return 0;
+
+		case 1:
+			return 2;
+
+		case 2:
+			switch (oper())
+			{
+				case static_cast<decltype(oper())>
+					(Vm::InstrGrp2Oper::Constu8):
+				case static_cast<decltype(oper())>
+					(Vm::InstrGrp2Oper::Consts8):
+					return 1;
+
+				case static_cast<decltype(oper())>
+					(Vm::InstrGrp2Oper::Constu16):
+				case static_cast<decltype(oper())>
+					(Vm::InstrGrp2Oper::Consts16):
+					return 2;
+
+				case static_cast<decltype(oper())>
+					(Vm::InstrGrp2Oper::Constu32):
+				case static_cast<decltype(oper())>
+					(Vm::InstrGrp2Oper::Consts32):
+					return 4;
+
+				case static_cast<decltype(oper())>
+					(Vm::InstrGrp2Oper::Const):
+					return 8;
+
+				default:
+					return 0;
+			}
+
+		case 3:
+		case 4:
+		default:
+			return 0;
+	}
+}
+
 Vm::Vm(size_t s_mem_amount, const std::string& s_code_filename)
 	: __mem_amount(s_mem_amount), __code_filename(s_code_filename)
 {
@@ -45,6 +91,86 @@ Vm::~Vm()
 int Vm::run()
 {
 	return 0;
+}
+
+u8 Vm::get_raw_mem8_at(Address where) const
+{
+	if (where < mem_amount())
+	{
+		return __mem[where];
+	}
+	else // if (where >= mem_amount)
+	{
+		printerr("Error:  get_raw_mem8_at():  address 0x", std::hex, where,
+			std::dec, " not in allocated memory range!\n");
+		exit(1);
+	}
+}
+u16 Vm::get_raw_mem16_at(Address where) const
+{
+	u16 ret = 0;
+	set_bits_with_range(ret, get_raw_mem8_at(where + 0), 15, 8);
+	set_bits_with_range(ret, get_raw_mem8_at(where + 1), 7, 0);
+	return ret;
+}
+u32 Vm::get_raw_mem32_at(Address where) const
+{
+	u32 ret = 0;
+	set_bits_with_range(ret, get_raw_mem8_at(where + 0), 31, 24);
+	set_bits_with_range(ret, get_raw_mem8_at(where + 1), 23, 16);
+	set_bits_with_range(ret, get_raw_mem8_at(where + 2), 15, 8);
+	set_bits_with_range(ret, get_raw_mem8_at(where + 3), 7, 0);
+	return ret;
+}
+u64 Vm::get_mem64_at(Address where) const
+{
+	u64 ret = 0;
+	set_bits_with_range(ret, get_raw_mem8_at(where + 0), 63, 56);
+	set_bits_with_range(ret, get_raw_mem8_at(where + 1), 55, 48);
+	set_bits_with_range(ret, get_raw_mem8_at(where + 2), 47, 40);
+	set_bits_with_range(ret, get_raw_mem8_at(where + 3), 39, 32);
+	set_bits_with_range(ret, get_raw_mem8_at(where + 4), 31, 24);
+	set_bits_with_range(ret, get_raw_mem8_at(where + 5), 23, 16);
+	set_bits_with_range(ret, get_raw_mem8_at(where + 6), 15, 8);
+	set_bits_with_range(ret, get_raw_mem8_at(where + 7), 7, 0);
+	return ret;
+}
+
+void Vm::set_mem8_at(Address where, u8 data)
+{
+	if (where < mem_amount())
+	{
+		__mem[where] = data;
+	}
+	else // if (where >= mem_amount)
+	{
+		printerr("Error:  set_mem8_at():  address 0x", std::hex, where,
+			std::dec, " not in allocated memory range!\n");
+		exit(1);
+	}
+}
+void Vm::set_mem16_at(Address where, u16 data)
+{
+	set_mem8_at(where + 0, data >> 8);
+	set_mem8_at(where + 1, data >> 0);
+}
+void Vm::set_mem32_at(Address where, u32 data)
+{
+	set_mem8_at(where + 0, data >> 24);
+	set_mem8_at(where + 1, data >> 16);
+	set_mem8_at(where + 2, data >> 8);
+	set_mem8_at(where + 3, data >> 0);
+}
+void Vm::set_mem64_at(Address where, u64 data)
+{
+	set_mem8_at(where + 0, data >> 56);
+	set_mem8_at(where + 1, data >> 48);
+	set_mem8_at(where + 2, data >> 40);
+	set_mem8_at(where + 3, data >> 32);
+	set_mem8_at(where + 4, data >> 24);
+	set_mem8_at(where + 5, data >> 16);
+	set_mem8_at(where + 6, data >> 8);
+	set_mem8_at(where + 7, data >> 0);
 }
 
 }
--- cake++_vm/src/vm_class.hpp
+++ cake++_vm/src/vm_class.hpp
@@ -13,7 +13,37 @@ class Vm
 public:		// constants
 	static constexpr size_t max_mem_amount = 0x100000000;
 
-public:		// enums
+public:		// types
+	typedef u64 Address;
+
+	class InstrHeader
+	{
+	private:		// variables
+		u8 __group, __oper;
+
+	public:		// functions
+		inline InstrHeader(u16 to_copy)
+		{
+			*this = to_copy;
+		}
+
+		inline InstrHeader(const InstrHeader& to_copy) = default;
+
+		inline InstrHeader& operator = (const InstrHeader& to_copy)
+			= default;
+		inline InstrHeader& operator = (u16 to_copy)
+		{
+			__group = to_copy >> 8;
+			__oper = to_copy;
+			return *this;
+		}
+
+		size_t num_arg_bytes() const;
+
+		gen_getter_by_val(group)
+		gen_getter_by_val(oper)
+	};
+
 	enum class InstrGrp0Oper : u8
 	{
 		Add,
@@ -115,6 +145,8 @@ private:		// variables
 
 	std::unique_ptr<u8[]> __mem;
 
+	Address __pc, __sp, __fp;
+
 public:		// functions
 	Vm(size_t s_mem_amount, const std::string& s_code_filename);
 	virtual ~Vm();
@@ -123,6 +155,43 @@ public:		// functions
 
 	gen_getter_by_val(mem_amount)
 	gen_getter_by_con_ref(code_filename)
+
+private:		// functions
+	u8 get_raw_mem8_at(Address where) const;
+	u16 get_raw_mem16_at(Address where) const;
+	u32 get_raw_mem32_at(Address where) const;
+	u64 get_mem64_at(Address where) const;
+
+	inline u64 get_casted_mem_u8_at(Address where) const
+	{
+		return static_cast<u64>(static_cast<u8>(get_raw_mem8_at(where)));
+	}
+	inline s64 get_casted_mem_s8_at(Address where) const
+	{
+		return static_cast<s64>(static_cast<s8>(get_raw_mem8_at(where)));
+	}
+	inline u64 get_casted_mem_u16_at(Address where) const
+	{
+		return static_cast<u64>(static_cast<u16>(get_raw_mem16_at(where)));
+	}
+	inline s64 get_casted_mem_s16_at(Address where) const
+	{
+		return static_cast<s64>(static_cast<s16>(get_raw_mem16_at(where)));
+	}
+	inline u64 get_casted_mem_u32_at(Address where) const
+	{
+		return static_cast<u64>(static_cast<u32>(get_raw_mem32_at(where)));
+	}
+	inline s64 get_casted_mem_s32_at(Address where) const
+	{
+		return static_cast<s64>(static_cast<s32>(get_raw_mem32_at(where)));
+	}
+
+
+	void set_mem8_at(Address where, u8 data);
+	void set_mem16_at(Address where, u16 data);
+	void set_mem32_at(Address where, u32 data);
+	void set_mem64_at(Address where, u64 data);
 };
 
 }
