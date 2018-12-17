975d76e54431e88937125fa7f2a2c5fa67e3d760
fl4shk <fl4shk@users.noreply.github.com>
2018-12-16 18:57:23 -0600

    tokens are now in an enum

--- src/lexer_class.hpp
+++ src/lexer_class.hpp
@@ -11,66 +11,100 @@ namespace cake_plus_plus
 
 class Lexer
 {
-private:		// functions
-	static inline const std::pair<ConstStrPtr, ConstStrPtr>
-		_make_token_map_entry(const std::string& key,
-		const std::string& value)
+public:		// enums
+	enum class Token
 	{
-		return std::pair<ConstStrPtr, ConstStrPtr>(unique_dup(key),
-			unique_dup(value));
-	}
-public:		// constants
-	static inline const std::map<ConstStrPtr, ConstStrPtr> TOKENS_MAP
-	= {
+		//MiscAuto,
+		MiscFunc,
+
+		// Punctuation
+		PunctLineComment,
+
+		PunctLParen,
+		PunctRParen,
+		PunctLBracket,
+		PunctRBracket,
+		PunctLBrace,
+		PunctRBrace,
+
+		PunctSemicolon,
+		PunctComma,
+
+
 		// Built-in Types
-		_make_token_map_entry("u8", "BuiltinTypeU8"),
-		_make_token_map_entry("s8", "BuiltinTypeS8"),
-		_make_token_map_entry("u16", "BuiltinTypeU16"),
-		_make_token_map_entry("s16", "BuiltinTypeS16"),
-		_make_token_map_entry("u32", "BuiltinTypeU32"),
-		_make_token_map_entry("s32", "BuiltinTypeS32"),
-		_make_token_map_entry("u64", "BuiltinTypeU64"),
-		_make_token_map_entry("s64", "BuiltinTypeS64"),
+		BuiltinTypeU8,
+		BuiltinTypeS8,
+		BuiltinTypeU16,
+		BuiltinTypeS16,
+		BuiltinTypeU32,
+		BuiltinTypeS32,
+		BuiltinTypeU64,
+		BuiltinTypeS64,
 
-		//_make_token_map_entry("float32", "BuiltinTypeFloat32"),
-		//_make_token_map_entry("float64", "BuiltinTypeFloat64"),
+		//BuiltinTypeFloat32,
+		//BuiltinTypeFloat64,
 
 
-		//_make_token_map_entry("auto", "Auto"),
 
-		_make_token_map_entry("func", "Func"),
-
-		_make_token_map_entry("//", "LineComment"),
 
 		// Arithmetic
-		_make_token_map_entry("+", "OpPlus"),
-		_make_token_map_entry("-", "OpMinus"),
-		_make_token_map_entry("*", "OpAsterisk"),
-		_make_token_map_entry("/", "OpDiv"),
-		_make_token_map_entry("%", "OpModulo"),
+		OpPlus,
+		OpMinus,
+		OpMul,
+		OpDiv,
+		OpModulo,
 
 
 		// Logical Unops
-		_make_token_map_entry("!", "OpLogNot"),
+		OpLogNot,
 
 		// Logical Binops
-		_make_token_map_entry("&&", "OpLogAnd"),
-		_make_token_map_entry("||", "OpLogOr"),
+		OpLogAnd,
+		OpLogOr,
 
 
 		// Bitwise Unops
-		_make_token_map_entry("~", "OpBitNot"),
+		OpBitNot,
 
 		// Bitwise Binops
-		_make_token_map_entry("&", "OpBitAnd"),
-		_make_token_map_entry("|", "OpBitOr"),
-		_make_token_map_entry("^", "OpBitXor"),
-		_make_token_map_entry("<<", "OpBitLsl"),
-		_make_token_map_entry(">>", "OpBitLsr"),
-		_make_token_map_entry(">>>", "OpBitAsr"),
+		OpBitAnd,
+		OpBitOr,
+		OpBitXor,
+		OpBitLsl,
+		OpBitLsr,
+		OpBitAsr,
+
+
+		// Comparisons
+		CmpEq,
+		CmpNe,
+		CmpLt,
+		CmpGe,
+		CmpLe,
+		CmpGt,
+
+		// Assignments
+		AssignRegular,
+		AssignPlus,
+		AssignMinus,
+		AssignMul,
+		AssignDiv,
+		AssignModulo,
+
+		AssignBitAnd,
+		AssignBitOr,
+		AssignBitXor,
+		AssignBitLsl,
+		AssignBitLsr,
+		AssignBitAsr,
 	};
 
+private:		// variables
+	
+
 public:		// functions
+	// Run the lexer
+	Token operator () ();
 
 };
 
